963f8740491bc092b55f60d6b8fac2a82986fa08
Ryan <rp_mcdonald@hotmail.com>
2018-09-06 20:06:42 -0700

    one

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+      <img alt="react" src="assets/images/carNutz.png" />
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Click the Car Game!</h1>
+    <h2>
+      <b>Click a Car for Points</b>
+    </h2>
+  </header>
+);
+
+export default Header;
